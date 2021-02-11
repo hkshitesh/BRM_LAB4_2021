9b1474634d400cc87470972d563846ac33847926
hksharma <hksharma@Hiteshksharma-LTP.mshome.net>
2021-02-11 09:55:07 +0530

    my third commit

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,11 +1,10 @@
 
 public class MyClass {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		
 		System.out.println("hello github and jenkins");
-
+		System.out.println("hello DevOps");
 	}
 
 }
